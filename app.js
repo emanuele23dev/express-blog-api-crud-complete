934e610448002869423d8304187431fa3e4769c3
emanuele23dev <emanuele.meggiarin@libero.it>
2024-11-06 16:34:18 +0100

    add show method

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const host = "http://127.0.0.1";
 
 const posts = require("./db/posts.js");
 
+const postsControllers = require('./controllers/postControllers.js')
+
 app.listen(port, () => {
   console.log(`App listen on ${host}:${port}`);
 });
@@ -15,20 +17,11 @@ app.get("/", (req, res) => {
   res.send("Express Blog Api Crud Complete");
 });
 
+
 // INDEX 
-app.get("/posts", );
+app.get("/posts", postsControllers.index);
+
+
 
 // SHOW 
-app.get("/posts/:title", (req, res) => {
-  const post = posts.find( post => post.title.toLowerCase() === req.params.title
-  );
-  // console.log(post);
-
-if (!post) {
-    return res.status(404).json({ error: `post con titolo ${req.params.title} non trovato` });
-  }
-
-  return res.status(200).json({
-    data: post
-  });
-});
+app.get("/posts/:title", postsControllers.show);
--- controllers/postControllers.js
+++ controllers/postControllers.js
@@ -8,7 +8,26 @@ const index = (req, res) => {
 };
 
 
+const show = (req, res) => {
+  const post = posts.find(
+    (post) => post.title.toLowerCase() === req.params.title
+  );
+  // console.log(post);
+
+  if (!post) {
+    return res
+      .status(404)
+      .json({ error: `post con titolo ${req.params.title} non trovato` });
+  }
+
+  return res.status(200).json({
+    data: post,
+  });
+};
+
+
 
 module.exports = {
-    index
+    index,
+    show
 }
